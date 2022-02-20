ec22cca7e1af775a53468935d29b927fea250402
HuangJinhui728 <18357515851@163.com>
2022-02-20 11:09:01 +0800

    login

--- src/main/java/com/hjhtest/community/controller/AuthorizeController.java
+++ src/main/java/com/hjhtest/community/controller/AuthorizeController.java
@@ -2,18 +2,17 @@ package com.hjhtest.community.controller;
 
 import com.hjhtest.community.data_transfer_object.AccessTokenDTO;
 import com.hjhtest.community.data_transfer_object.GithubUserDTO;
-
 import com.hjhtest.community.mapper.UserMapper;
 import com.hjhtest.community.model.User;
 import com.hjhtest.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -41,7 +40,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -53,18 +52,17 @@ public class AuthorizeController {
         GithubUserDTO githubUserDTO = githubProvider.getUser(accessToken);
 
         if(githubUserDTO != null){
-            //登陆成功 写cookie 和 session
+
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUserDTO.getName());
             user.setAccountId(String.valueOf(githubUserDTO.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-
-
-
-            request.getSession().setAttribute("user",githubUserDTO);
+            response.addCookie(new Cookie("token",token));
+            //登陆成功 写cookie 和 session
             return "redirect:/";
         } else{
             //登陆失败，重新登录
--- src/main/java/com/hjhtest/community/controller/IndexController.java
+++ src/main/java/com/hjhtest/community/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.hjhtest.community.controller;
 
+import com.hjhtest.community.mapper.UserMapper;
+import com.hjhtest.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
 
 
 /**
@@ -17,8 +23,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies =request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user =userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+
+        }
+
         return "index";
     }
 
